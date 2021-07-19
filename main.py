49385720bfa87a73053e2a2562c42daa3ee14c9e
MartinsMessias <messiasads2017@gmail.com>
2021-07-19 16:52:15 -0500

    Refactoring started

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
-from os import abort
+from flask import Flask, request, jsonify, abort
 
-from flask import Flask, request, jsonify
-import json
-from minesManager import MinesManager
-from minesChecker import MinesChecker
+from utils.minesManager import MinesManager
 
 app = Flask(__name__)
 
 manager = MinesManager()
-checker = MinesChecker()
-generatedJson = {}
 
 
 @app.route('/')
 def index():
-    a = None
     return app.send_static_file('index.html')
 
 
 @app.route('/newGameData')
 def new_game_data():
-    generated_json = manager.getMines()
+    # if not request.json:
+    #     abort(400)
+    request_data = request.get_json()
+    width = 5 #request_data['width']
+    height = 5 #request_data['height']
+    count_of_mines = 5 #request_data['countOfMines']
+    generated_json = manager.getMines(width, height, count_of_mines)
+    print(generated_json)
     return jsonify(generated_json)
 
 
-@app.route('/postMoveData', methods=['POST'])
-def post_move_data():
-    if not request.json:
-        abort(400)
-    ret = checker.checkMines(request.json)
-    return jsonify(ret)
+# @app.route('/postMoveData', methods=['POST'])
+# def post_move_data():
+#     if not request.json:
+#         abort(400)
+#     ret = checker.checkMines(request.json)
+#     return jsonify(ret)
 
 
 if __name__ == '__main__':
--- utils/minesMaker.py
+++ utils/minesMaker.py
@@ -6,35 +6,37 @@ def __createEmptyTable(width, height):
     ret = []
     line = []
     for i in range(0, width):
-        line.append({'type': 'empty', 'clickable': True, 'marked': False})
+        line.append(0)
     for i in range(0, height):
         ret.append(deepcopy(line))
     return ret
 
+
 def __setRandomMines(table, countOfMines):
     i = 0
-    while (i < countOfMines):
+    while i < countOfMines:
         randY = randint(0, len(table) - 1)
         randX = randint(0, len(table[0]) - 1)
-        if table[randY][randX]['type'] == 'mine':
+        if table[randY][randX] == 'mine':
             i -= 1
         else:
-            table[randY][randX]['type'] = 'mine'
+            table[randY][randX] = 'mine'
         i += 1
     return table
+
 
 def __fillFields(table):
     for y in range(0, len(table)):
         for x in range(0, len(table[0])):
-            if not table[y][x]['type'] == 'mine':
+            if not table[y][x] == 'mine':
                 count = 0
                 for i in range(-1, 2):
                     for j in range(-1, 2):
-                        if 0 <= y+i < len(table) and 0 <= x+j < len(table[0]):
-                            if (table[y + i][x + j]['type'] == 'mine'):
+                        if 0 <= y + i < len(table) and 0 <= x + j < len(table[0]):
+                            if table[y + i][x + j] == 'mine':
                                 count += 1
                 if count > 0:
-                    table[y][x]['type'] = count
+                    table[y][x] = count
     return table
 
 
