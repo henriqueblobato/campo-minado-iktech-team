a6660f7ab820617a7b2d822c212454185c65f8a1
henrique.lobato <henrique.lobato@swimlane.com>
2021-07-20 11:59:35 -0300

    add: /postMoveData

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 import json
 
 from utils.mines_functions import create_matrix, create_random_mines, create_number_fields
+from utils.MinesChecker import MinesChecker
+checker = MinesChecker()
 
 app = Flask(__name__)
 
@@ -11,13 +13,13 @@ def index():
     return app.send_static_file('index.html')
 
 
-@app.route('/newGameData', methods=['POST'])
+@app.route('/newGameData', methods=['POST', 'GET'])
 def new_game_data():
     request_data = request.get_json()
     try:
-        width = min(20, int(request_data['width']))
-        height = min(20, int(request_data['height']))
-        mines = min(width*height, int(request_data['countOfMines']))
+        width = min(20, int(request_data['rows']))
+        height = min(20, int(request_data['columns']))
+        mines = min(width*height, int(request_data['mines']))
     except Exception as e:
         print(f'{type(e)}, {str(format(e))}')
         width, height, mines = 5, 5, 5
@@ -26,7 +28,14 @@ def new_game_data():
     response = create_random_mines(response, mines)
     response = create_number_fields(response)
 
+    print(response)
     return json.dumps(response)
+
+
+@app.route('/postMoveData', methods=['POST'])
+def post_move_data():
+    ret = checker.checkMines(request.json)
+    return json.dumps(ret)
 
 
 if __name__ == '__main__':
